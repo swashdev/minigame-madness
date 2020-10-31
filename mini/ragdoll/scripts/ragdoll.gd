a587b05ad4d1250a700408427daa7e6bdf83fe20
Philip Pavlick <swashdev@pm.me>
2020-10-31 16:43:46 -0500

    add scripting for player input to trigger failure
    A new function triggers an animation in the `FluffyRagdoll` and freezes
    user input.  This function is called upon user input with a parameter
    used to determine which "fall over" animation it should use.
    
    As a placeholder, if the user presses a key for which an animation is
    not yet available, Fluffy simply flips horizontally.
    
    Whether or not the `decide` function signals "won" or "lost" now depends
    on whether the game has detected a key pressed (i.e. the `key_pressed`
    variable is set to `true`)

--- mini/ragdoll/scripts/ragdoll.gd
+++ mini/ragdoll/scripts/ragdoll.gd
@@ -5,19 +5,42 @@ extends "res://shared/scripts/minigame.gd"
 class_name Ragdoll
 
 
+# A variable which stores whether or not the player has pressed a key
+var key_pressed: bool = false
+
+
 # In this particular game, if the timer has gotten all the way down to the
 # bottom, that means that the player has won by default
 func decide():
-	emit_signal( "won" )
-
-
-# Upon detecting an input (keyboard stroke) from the player, immediately
-# signal a loss
-func _input( event ):
-	if event is InputEventKey:
+	if key_pressed:
 		emit_signal( "lost" )
+	else:
+		emit_signal( "won" )
 
 
-# Philip, fill me in later.
+# A function which starts Fluffy's "fall over" animations and freezes user
+# input
+func tip_fluffy_over( direction ):
+	$FluffyRagdoll.animation = direction
+	$FluffyRagdoll.play()
+	
+	key_pressed = true
+
+
+# The process for this game is very simple: if the player presses a key, they
+# lose
 func _process( _delta ):
-	pass
+	# Do nothing if the player has already pressed a key
+	if !key_pressed:
+		# Play a little animation depending on which specific key was
+		# pressed
+		if Input.is_action_pressed( "ui_right" ) \
+		or Input.is_action_pressed( "ui_up" ):
+			tip_fluffy_over( "fall_right" )
+		elif Input.is_action_pressed( "ui_left" ) \
+		or Input.is_action_pressed( "ui_down" ) \
+		or Input.is_action_pressed( "ui_select" ):
+			# Placeholder event for animations I haven't completed
+			# yet:
+			$FluffyRagdoll.flip_h = true
+			key_pressed = true
