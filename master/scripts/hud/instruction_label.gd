933e9c6457c918fc83b49e4f44f597ac24726c05
Philip Pavlick <swashdev@pm.me>
2020-11-05 17:58:59 -0600

    add scripting for the in-game HUD label
    A timer has been added to the Label from the in-game HUD, and a script
    has been penned which handles animations.

---
+++ master/scripts/hud/instruction_label.gd
@@ -0,0 +1,55 @@
+# `instruction_label.gd`: Provides special functions for the Label in the
+# in-game HUD.  Mostly used for animations.
+
+
+extends Label
+
+
+# Possible labels for the animation that the label is currently doing.
+enum LabelAnimation \
+{
+	NONE,
+	ZOOM_IN_FROM_RIGHT,
+	ZOOM_OUT_LEFT,
+}
+
+
+# The label's current animation.
+var current_animation = LabelAnimation.NONE
+
+
+# Triggers an animation in order to display a message.
+func zoom_in_from_right( message = "your message here!" ):
+	text = message
+	rect_position.x = 640
+	visible = true
+	current_animation = LabelAnimation.ZOOM_IN_FROM_RIGHT
+
+
+func _process(delta):
+	if current_animation == LabelAnimation.ZOOM_IN_FROM_RIGHT:
+		# The label will zoom in from the right, settling when its x value
+		# reaches 0
+		if rect_position.x >= 160:
+			rect_position.x -= 160 * delta
+		# When the label reaches the left side of the screen, snap the value
+		# just in case we overshot and then start a timer to linger in this
+		# position.
+		else:
+			rect_position.x = 0
+			current_animation = LabelAnimation.NONE
+			$MessageLingerTimer.start()
+	elif current_animation == LabelAnimation.ZOOM_OUT_LEFT:
+		# The label will zoom out to the left, stopping and becoming invisible
+		# after it is fully off the screen.
+		if rect_position.x > -640:
+			rect_position.x -= 160 * delta
+		else:
+			visible = false
+			current_animation = LabelAnimation.NONE
+
+
+# A timer used to determine how long a message should remain on-screen before
+# zooming off-screen.
+func _on_MessageLingerTimer_timeout():
+	current_animation = LabelAnimation.ZOOM_OUT_LEFT
