58cb9e54882cff557029a30a416a0f2e27e6e7e4
Philip Pavlick <swashdev@pm.me>
2020-11-06 12:12:03 -0600

    allow either animation for messages.
    The `message` function now takes a parameter `animation` whose default
    value is `MESSAGE_FROM_BOTTOM` which will determine which animation will
    be used to animate messages in the HUD.
    
    This way we can simply expand the `message` function to allow for more
    animations if I think of any, as long as they're not super complex.

--- master/scripts/in-game_HUD.gd
+++ master/scripts/in-game_HUD.gd
@@ -5,6 +5,13 @@ extends Control
 
 signal message_exited
 
+enum \
+{
+	MESSAGE_FROM_BOTTOM,
+	MESSAGE_FROM_RIGHT,
+	# Leading comma on last entry intentional for diff files.
+}
+
 
 func show_life_counter():
 	$LifeCounter.visible = false
@@ -35,7 +42,11 @@ func update_progress_bar( value ):
 
 # Display a given message using an animation defined in the `InstructionLabel`
 # script and then signal when the message is off-screen.
-func message( text ):
-	$InstructionLabel.zoom_in_from_bottom( text )
+func message( text, animation = MESSAGE_FROM_BOTTOM ):
+	match animation:
+		MESSAGE_FROM_RIGHT:
+			$InstructionLabel.zoom_in_from_right( text )
+		_, MESSAGE_FROM_BOTTOM:
+			$InstructionLabel.zoom_in_from_bottom( text )
 	yield( $InstructionLabel, "animation_finished" )
 	emit_signal( "message_exited" )
