0b57161e81f0d7fe08488bd0485621e47da9ba6a
Philip Pavlick <swashdev@pm.me>
2020-11-06 12:00:18 -0600

    better scripting for the `InstructionLabel`
    The instruction label can now use the `MessageLingerTimer` to transition
    between multiple animations rather than having the single hard-coded
    behavior.  In keeping with this, a new animation ZOOM_IN_FROM_BOTTOM has
    been added, with an accompanying ZOOM_OUT_TOP.

--- master/scripts/hud/instruction_label.gd
+++ master/scripts/hud/instruction_label.gd
@@ -10,11 +10,19 @@ enum LabelAnimation \
 {
 	NONE,
 	ZOOM_IN_FROM_RIGHT,
+	ZOOM_IN_FROM_BOTTOM,
 	ZOOM_OUT_LEFT,
+	ZOOM_OUT_TOP,
 }
 
-# The change in x per second when an animation is playing.
+# The change in x per second when a horizontal "zoom" animation is playing.
 const MESSAGE_ZOOM_DX: int = 1280
+# The change in y per second when a vertical "zoom" animation is playing.
+const MESSAGE_ZOOM_DY: int = 960
+# The x value for which the message will be considered to be "centered"
+const MESSAGE_CENTER_X: int = 0
+# The y value for same.
+const MESSAGE_CENTER_Y: int = 233
 
 # The label's current animation.
 var _current_animation = LabelAnimation.NONE
@@ -24,27 +32,49 @@ func _process(delta):
 	if _current_animation == LabelAnimation.ZOOM_IN_FROM_RIGHT:
 		# The label will zoom in from the right, settling when its x value
 		# reaches 0
-		if rect_position.x >= MESSAGE_ZOOM_DX * delta:
+		if rect_position.x - MESSAGE_CENTER_X >= MESSAGE_ZOOM_DX * delta:
 			rect_position.x -= MESSAGE_ZOOM_DX * delta
 		# When the label reaches the left side of the screen, snap the value
 		# just in case we overshot and then start a timer to linger in this
 		# position.
 		else:
-			rect_position.x = 0
+			rect_position.x = MESSAGE_CENTER_X
 			_current_animation = LabelAnimation.NONE
+			# Linger for a moment and then set the animation to ZOOM_OUT_LEFT.
 			$MessageLingerTimer.start()
+			yield( $MessageLingerTimer, "timeout" )
+			_current_animation = LabelAnimation.ZOOM_OUT_LEFT
 	elif _current_animation == LabelAnimation.ZOOM_OUT_LEFT:
 		# The label will zoom out to the left, stopping and becoming invisible
 		# after it is fully off the screen.
-		if rect_position.x > -640:
+		if rect_position.x > -1 * rect_size.x:
 			rect_position.x -= MESSAGE_ZOOM_DX * delta
 		else:
 			visible = false
 			_current_animation = LabelAnimation.NONE
 			emit_signal( "animation_finished" )
 
+	elif _current_animation == LabelAnimation.ZOOM_IN_FROM_BOTTOM:
+		if rect_position.y - MESSAGE_CENTER_Y >= MESSAGE_ZOOM_DY * delta:
+			rect_position.y -= MESSAGE_ZOOM_DY * delta
+		else:
+			rect_position.y = MESSAGE_CENTER_Y
+			_current_animation = LabelAnimation.NONE
+			# Linger for a moment and then set thte animation to ZOOM_OUT_TOP
+			$MessageLingerTimer.start()
+			yield( $MessageLingerTimer, "timeout" )
+			_current_animation = LabelAnimation.ZOOM_OUT_TOP
+	elif _current_animation == LabelAnimation.ZOOM_OUT_TOP:
+		if rect_position.y > -1 * rect_size.y:
+			rect_position.y -= MESSAGE_ZOOM_DY * delta
+		else:
+			visible = false
+			_current_animation = LabelAnimation.NONE
+			emit_signal( "animation_finished" )
 
-# Triggers an animation in order to display a message.
+
+# Causes a message to zoom in from the right of the screen and then exit
+# to the left.
 func zoom_in_from_right( message = "your message here!" ):
 	text = message
 	rect_position.x = 640
@@ -52,7 +82,10 @@ func zoom_in_from_right( message = "your message here!" ):
 	_current_animation = LabelAnimation.ZOOM_IN_FROM_RIGHT
 
 
-# A timer used to determine how long a message should remain on-screen before
-# zooming off-screen.
-func _on_MessageLingerTimer_timeout():
-	_current_animation = LabelAnimation.ZOOM_OUT_LEFT
+# Causes a message to zoom in from the bottom of the screen and then exit
+# through the top.
+func zoom_in_from_bottom( message = "your message here!" ):
+	text = message
+	rect_position.y = 480
+	visible = true
+	_current_animation = LabelAnimation.ZOOM_IN_FROM_BOTTOM
