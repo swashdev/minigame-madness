3005a05522912517c0d3dd9bfeeb910e59ab58c9
Philip Pavlick <swashdev@pm.me>
2020-11-06 10:55:24 -0600

    make `animate_key` a public function.
    It makes more sense for this function to be public than not, so do that.
    The other functions are just shortcuts anyway.

--- shared/scripts/ui/keys/arrow_keys.gd
+++ shared/scripts/ui/keys/arrow_keys.gd
@@ -8,7 +8,7 @@ class_name ArrowKeys
 
 # instructs the given arrow to do a certain animation.  if `which_key` is
 # "all," apply the animation to all of the keys.
-func _animate_key( which_animation, which_key = "all" ):
+func animate_key( which_animation, which_key = "all" ):
 	if which_key == "all" or which_key == "up":
 		$UpKey.animate( which_animation )
 	if which_key == "all" or which_key == "down":
@@ -22,19 +22,19 @@ func _animate_key( which_animation, which_key = "all" ):
 # lights up the given key by setting it to its default animation, or all of
 # the keys, as appropriate.
 func light( which_key = "all" ):
-	_animate_key( "default", which_key )
+	animate_key( "default", which_key )
 
 
 # dims the given key, or all of them, as appropriate.
 func dim( which_key = "all" ):
-	_animate_key( "dim", which_key )
+	animate_key( "dim", which_key )
 
 
 # causes the given key to blink, or all of them, as appropriate.
 func blink( which_key = "all" ):
-	_animate_key( "blink", which_key )
+	animate_key( "blink", which_key )
 
 
 # causes the given key to blink rapidly, or all of them, as appropriate.
 func blink_rapidly( which_key = "all" ):
-	_animate_key( "blink_rapidly", which_key )
+	animate_key( "blink_rapidly", which_key )
