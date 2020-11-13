c5687634ad4573bcd51dedd93394ade4b85e45a2
Philip Pavlick <swashdev@pm.me>
2020-11-13 15:55:11 -0600

    add a VisibilityNotifier2D to class Projectile2D.
    For some reason I was under the impression from the tutorials I've read
    that the VisibilityNotifier2D was a requirement for a RigidBody2D which
    would be added by the inheriting class, but this isn't the case, so I'm
    adding it to the base node.

--- shared/scripts/projectiles/projectile.gd
+++ shared/scripts/projectiles/projectile.gd
@@ -4,12 +4,6 @@ extends RigidBody2D
 # shape, and size.
 
 
-# Connect signals from children to relevant functions.
-func _ready():
-	$VisibilityNotifier2D.connect( "screen_exited", self, \
-			"_on_VisibilityNotifier2D_screen_exited" )
-
-
 # Disappear when leaving the screen.
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
