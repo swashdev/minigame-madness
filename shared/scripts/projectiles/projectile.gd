e7becf11a870a1a04dc7e5d4260344bf11b37f85
Philip Pavlick <swashdev@pm.me>
2020-11-13 16:22:49 -0600

    add a `set_velocity` function to Projectile2D.
    The `set_velocity` function will set the speed & direction of the
    projectile and recalculate its linear velocity.  The `speed` &
    `direction` variables have been made private, and their setter & getter
    functions have been altered to recalculate the linear velocity while
    they are being set.

--- shared/scripts/projectiles/projectile.gd
+++ shared/scripts/projectiles/projectile.gd
@@ -5,10 +5,10 @@ extends RigidBody2D
 
 
 # The rate of speed of the projectile, in pixels.
-export (float) var speed = 0.0 setget set_speed, get_speed
+export (float) var _speed = 0.0 setget set_speed, get_speed
 
 # The direction that the projectile faces, in degrees.
-export (float) var direction = 0.0 setget set_direction, get_direction
+export (float) var _direction = 0.0 setget set_direction, get_direction
 
 # The rate of spin for the projectile in degrees.
 export (float) var spin_degrees = 0.0 setget set_spin, get_spin
@@ -21,20 +21,22 @@ func _process( delta ):
 		rotation_degrees += spin_degrees * delta
 
 
+# Sets the projectile's speed and recalculates its linear velocity.
 func set_speed( new_speed: float ):
-	speed = new_speed
+	set_velocity( new_speed )
 
 
 func get_speed() -> float:
-	return speed
+	return _speed
 
 
+# Sets the projectile's direction and recalculates its linear velocity.
 func set_direction( new_direction: float ):
-	direction = new_direction
+	set_velocity( _speed, new_direction )
 
 
 func get_direction() -> float:
-	return direction
+	return _direction
 
 
 func set_spin( new_spin: float ):
@@ -43,6 +45,14 @@ func set_spin( new_spin: float ):
 
 func get_spin() -> float:
 	return spin_degrees
+
+
+# Recalculates the projectile's linear velocity based on the `speed` &
+# `direction`
+func set_velocity( new_speed = _speed, new_direction = _direction ):
+	_speed = new_speed
+	_direction = new_direction
+	linear_velocity = Vector2( _speed, 0 ).rotated( deg2rad( _direction ) )
 
 
 # Disappear when leaving the screen.
