3a9fbefd6d36c795468c5c5286ffe55387ed8b47
Philip Pavlick <swashdev@pm.me>
2020-11-13 16:11:20 -0600

    add `speed` & `direction` vars to Projectile2D.
    The `speed` and `direction` variables will be used to determine the
    velocity vector for the projectile.

--- shared/scripts/projectiles/projectile.gd
+++ shared/scripts/projectiles/projectile.gd
@@ -4,8 +4,37 @@ extends RigidBody2D
 # shape, and size.
 
 
+# The rate of speed of the projectile, in pixels.
+export (float) var speed = 0.0 setget set_speed, get_speed
+
+# The direction that the projectile faces, in degrees.
+export (float) var direction = 0.0 setget set_direction, get_direction
+
 # The rate of spin for the projectile in degrees.
 export (float) var spin_degrees = 0.0 setget set_spin, get_spin
+
+
+# The default process will move the projectile in space and, if applicable,
+# spin it.
+func _process( delta ):
+	if spin_degrees != 0.0:
+		rotation_degrees += spin_degrees * delta
+
+
+func set_speed( new_speed: float ):
+	speed = new_speed
+
+
+func get_speed() -> float:
+	return speed
+
+
+func set_direction( new_direction: float ):
+	direction = new_direction
+
+
+func get_direction() -> float:
+	return direction
 
 
 func set_spin( new_spin: float ):
@@ -14,13 +43,6 @@ func set_spin( new_spin: float ):
 
 func get_spin() -> float:
 	return spin_degrees
-
-
-# The default process will move the projectile in space and, if applicable,
-# spin it.
-func _process( delta ):
-	if spin_degrees != 0.0:
-		rotation_degrees += spin_degrees * delta
 
 
 # Disappear when leaving the screen.
