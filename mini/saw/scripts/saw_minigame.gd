819e739bf85546a634b737c23cba28dd05e4af9a
Philip Pavlick <swashdev@pm.me>
2020-11-13 12:51:41 -0600

    animate the saws falling apart after winning.

--- mini/saw/scripts/saw_minigame.gd
+++ mini/saw/scripts/saw_minigame.gd
@@ -6,10 +6,18 @@ extends Minigame
 const SAW_DX = 50
 const SAW_DY = 2
 
+# Constants relevant to the falling logs animation.
+const LOG_DX = 100
+const LOG_DY = 400
+const LOG_DR = 180
+
+# Whether or not to animate the logs falling.
+var _logs_fall_everyone_dies : bool = false
+
 
 # The process for this game is very simple: The player must alternate the left
 # and right keys to keep the saw moving down until the log splits.
-func _process( _delta ):
+func _process( delta ):
 	# Check for player input.
 	if _unlock_controls:
 		if Input.is_action_pressed( "ui_left" ):
@@ -31,7 +39,18 @@ func _process( _delta ):
 		# it has, signal victory and lock the controls; we're done.
 		if $Saw.position.y >= $Target.position.y:
 			_unlock_controls = false
+			$SeamCover.set_visible( false )
+			_logs_fall_everyone_dies = true
 			emit_signal( "won" )
+
+	# Animate the logs falling away.
+	if _logs_fall_everyone_dies:
+		$LogFore.position.x += LOG_DX * delta
+		$LogFore.position.y += LOG_DY * delta
+		$LogFore.rotation_degrees += LOG_DR * delta
+		$LogAft.position.x -= LOG_DX * delta
+		$LogAft.position.y += LOG_DY * delta
+		$LogAft.rotation_degrees -= LOG_DR * delta
 
 
 # Begin the minigame.
