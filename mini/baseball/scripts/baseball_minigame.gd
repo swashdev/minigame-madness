7c7797a286b14217cb0a9d9702a9afcf08cab59e
Philip Pavlick <swashdev@pm.me>
2020-11-13 18:05:22 -0600

    fix collision detection.
    Screw this crap with collision objects, just use coordinates to
    determine whether the player hits or not.

--- mini/baseball/scripts/baseball_minigame.gd
+++ mini/baseball/scripts/baseball_minigame.gd
@@ -13,6 +13,19 @@ func _process( _delta ):
 				_swung = true
 				$Batter.animation = "swing"
 				$Batter.play()
+				# The animation is so fast that I'm not even going to wait
+				# for it to finish; we'll assume win state relies on player's
+				# reflexes, not the animation speed.
+				# Check what collision detection bodies the ball is hitting
+				# right now.
+				var ball_x = $Baseball.position.x
+				if ball_x > $SwingMax.position.x \
+				and ball_x < $SwingMin.position.x:
+					emit_signal( "won" )
+					$Baseball.set_direction( -45.0 )
+				else:
+					$Pitcher/Timer.stop()
+					emit_signal( "lost" )
 
 
 # Unlock the controls & start the pitcher's timer.
