3ec7769d77480701f9358272a012f65076a93275
Philip Pavlick <swashdev@pm.me>
2020-11-14 15:23:11 -0600

    expand the `explode` function for the HalfPizza.
    The `HalfPizza` class's `explode` function will now create two `Pizza`
    instances with a linear velocity similar to the original `HalfPizza`'s
    before exploding.

---
+++ mini/anchovy_quest/scripts/obstacles/half_pizza.gd
@@ -0,0 +1,26 @@
+class_name HalfPizza
+extends Pizza
+# A half-pizza combining two pizza slices.
+
+
+# This scene acts as a template for the pizzas that come out of the HalfPizza
+# when it explodes.
+export (PackedScene) var _slice
+
+
+# This `explode` function expands on the explosion from `Pizza` by creating
+# two `Pizza` slices which emerge from the exploding `HalfPizza`
+func explode():
+	var slice1 = _slice.instance()
+	var slice2 = _slice.instance()
+	get_parent().add_child( slice1 )
+	get_parent().add_child( slice2 )
+
+	slice1.position = position
+	slice2.position = position
+
+	# Adjust the velocities of the two slices by rotating each of them.
+	slice1.linear_velocity = linear_velocity.rotated( deg2rad( 45.0 ) )
+	slice2.linear_velocity = linear_velocity.rotated( deg2rad( -45.0 ) )
+
+	.explode()
