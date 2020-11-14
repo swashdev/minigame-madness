177059b627b08b1a3795a0b43d16489cfda9e404
Philip Pavlick <swashdev@pm.me>
2020-11-14 15:56:45 -0600

    update the player script.
    The player script is now written with the new collision layers and masks
    in mind.

--- mini/anchovy_quest/scripts/characters/anchovy.gd
+++ mini/anchovy_quest/scripts/characters/anchovy.gd
@@ -8,10 +8,8 @@ signal hit
 
 # Detect collisions with obstacles and handle them appropriately.
 func _on_Anchovy_body_entered( body ):
-	# Only emit the "hit" signal if the body is an obstacle.
-	if body extends Pizza:
-		# Show the "ouch" sprite and emit the "hit" signal to signal the
-		# minigame that the player's death sequence needs to be started.
-		$Sprite.set_visible( false )
-		$BoomSprite.set_visible( true )
-		emit_signal( "hit" )
+	# Show the "ouch" sprite and emit the "hit" signal to signal the
+	# minigame that the player's death sequence needs to be started.
+	$Sprite.set_visible( false )
+	$BoomSprite.set_visible( true )
+	emit_signal( "hit" )
