1f531140796aeb698b2a3710d70037e2db7fe1e4
Philip Pavlick <swashdev@pm.me>
2020-11-14 14:43:39 -0600

    add a function to explode the pizza.
    The `explode` function will spawn an `Explosion` scene at the pizza
    slice's coordinates and then remove it from the queue, destroying the
    pizza.  Naturally this will have to be overwritten by the `HalfPizza`
    and `WholePizza` scenes to cause it to also spawn smaller pizzas.

--- mini/anchovy_quest/scripts/obstacles/pizza.gd
+++ mini/anchovy_quest/scripts/obstacles/pizza.gd
@@ -3,6 +3,10 @@ extends RigidBody2D
 # A colliding pizza that flies through space trying to smoosh anchovies.
 
 
+# This variable contains the pizza's "Explosion" scene, which it spawns in as
+# its explosion animation.
+export (PackedScene) var _explosion
+
 # Determines how quickly the pizza's sprite and collision polygon will spin.
 var spin_degrees: float = 0.0 setget set_spin, get_spin
 
@@ -22,3 +26,21 @@ func set_spin( new_spin: float ):
 
 func get_spin() -> float:
 	return spin_degrees
+
+
+# This function causes the pizza slice to explode by spawning an `Explosion`
+# and then exiting the queue.
+func explode():
+	var explosion = _explosion.instance()
+	# Add the explosion scene to the parent tree, so that it will persist
+	# after this node has been freed from the queue.
+	get_parent().add_child( explosion )
+
+	# Set the explosion's coordinates to the coordinates for the current node.
+	explosion.position = position
+	# Play the explosion's animation, which will trigger the rest of its
+	# script.
+	explosion.play()
+
+	# Exit stage none.
+	queue_free()
