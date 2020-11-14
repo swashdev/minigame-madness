109bc4e5c132bf83135633ffabce4b3cd9a8cd68
Philip Pavlick <swashdev@pm.me>
2020-11-14 17:14:00 -0600

    only detect collisions if the player can move.
    If movement has been disabled, do not allow the player character to die.

--- mini/anchovy_quest/scripts/characters/anchovy.gd
+++ mini/anchovy_quest/scripts/characters/anchovy.gd
@@ -61,8 +61,9 @@ func _recalc_velocity():
 
 # Detect collisions with obstacles and handle them appropriately.
 func _on_Anchovy_body_entered( body ):
-	# Show the "ouch" sprite and emit the "hit" signal to signal the
-	# minigame that the player's death sequence needs to be started.
-	$Sprite.set_visible( false )
-	$BoomSprite.set_visible( true )
-	emit_signal( "hit" )
+	if allow_movement:
+		# Show the "ouch" sprite and emit the "hit" signal to signal the
+		# minigame that the player's death sequence needs to be started.
+		$Sprite.set_visible( false )
+		$BoomSprite.set_visible( true )
+		emit_signal( "hit" )
