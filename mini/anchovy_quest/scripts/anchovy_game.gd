db1c6e30c873db606272ccadbc093ee34b73c9da
Philip Pavlick <swashdev@pm.me>
2020-11-14 19:17:15 -0600

    disable the pepperoni's collision until needed.

--- mini/anchovy_quest/scripts/anchovy_game.gd
+++ mini/anchovy_quest/scripts/anchovy_game.gd
@@ -66,3 +66,4 @@ func _on_Player_shoot( location, direction ):
 	bullet.position = location
 	# Set the bullet's direction appropriately.
 	bullet.set_direction( direction )
+	bullet.CollisionShape2D.disabled = false
