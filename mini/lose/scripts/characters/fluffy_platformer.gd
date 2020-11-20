5485c35a924f3a3a9d8175314443ffdc5284ae00
Philip Pavlick <swashdev@pm.me>
2020-11-20 12:51:27 -0600

    implement basic movement for FluffyPlatformer.

--- mini/lose/scripts/characters/fluffy_platformer.gd
+++ mini/lose/scripts/characters/fluffy_platformer.gd
@@ -2,6 +2,32 @@ extends RigidBody2D
 # A scene for the Fluffy platformer character in the "Lose!" minigame.
 
 
+# Fluffy's movement speed.
+const MOVE_SPEED: float = 400.0
+
+
+# Fluffy's mainloop.
+func _process( delta ):
+	# Move left & right according to presses of the left & right keys.
+	if Input.is_action_pressed( "ui_right" ):
+		position.x += MOVE_SPEED * delta
+		$Sprite.animation = "run"
+		$Sprite.play()
+	elif Input.is_action_pressed( "ui_left" ):
+		position.x -= MOVE_SPEED * delta
+		$Sprite.animation = "run"
+		$Sprite.play()
+	else:
+		# Stop animations if no inputs are pressed.
+		$Sprite.animation = "default"
+
+	# Adjust sprite orientation based on inputs.
+	if Input.is_action_just_pressed( "ui_right" ):
+		flip( false )
+	if Input.is_action_just_pressed( "ui_left" ):
+		flip( true )
+
+
 # Horizontally flips Fluffy's sprite.
 func flip( yn: bool ):
 	# Fluffy's sprite is already flipped, so we do the opposite of what the
