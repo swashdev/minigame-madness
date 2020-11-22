de567a6a926e7f117c450951e2d5619f41232c7d
Philip Pavlick <swashdev@pm.me>
2020-11-21 20:15:36 -0600

    [ZA] adjust Slicer script to disallow movement.
    The minigame will have to unlock the slicer's movement.

--- mini/za/scripts/slicer/slicer.gd
+++ mini/za/scripts/slicer/slicer.gd
@@ -5,10 +5,14 @@ extends Node2D
 # How quickly the slicer rotates along its axis in degrees per second.
 const ROTATE_SPEED: float = 180.0
 
+# Whether or not to allow movement.
+var allow_movement: bool = false
+
 
 # The slicer's mainloop.
 func _process( delta ):
-	if Input.is_action_pressed( "move_right" ):
-		rotation_degrees += ROTATE_SPEED * delta
-	elif Input.is_action_pressed( "move_left" ):
-		rotation_degrees -= ROTATE_SPEED * delta
+	if allow_movement:
+		if Input.is_action_pressed( "move_right" ):
+			rotation_degrees += ROTATE_SPEED * delta
+		elif Input.is_action_pressed( "move_left" ):
+			rotation_degrees -= ROTATE_SPEED * delta
