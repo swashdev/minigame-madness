4df6b8101bc9ac6b4f8ba03108a665e8d6af54fa
Philip Pavlick <swashdev@pm.me>
2020-11-22 13:53:57 -0600

    [BIGRIG] add a script for Rig1.
    An initial draft of the script for Rig1 has been written.  If the right
    key is pressed, it will accelerate and move along the path according to
    its instantaneous velocity.

---
+++ mini/big_rigs/scripts/characters/rig.gd
@@ -0,0 +1,38 @@
+extends PathFollow2D
+# A script for the Rigs in the "Race that Rig!" minigame.
+
+
+# The max speed of the rig.
+const MAX_SPEED: float = 100.0
+
+var allow_movement: bool = true
+
+# How quickly the rig is moving.
+var _speed: float = 0.0
+
+# The rate of acceleration per second.
+var _acceleration: float = 10.0
+
+
+# The mainloop for the rig.
+func _process( delta ):
+	if allow_movement:
+		# Player input affects the rate at which the rig will move.
+		if Input.is_action_pressed( "move_right" ):
+			if _speed < MAX_SPEED:
+				_speed += _acceleration * delta
+		elif Input.is_action_pressed( "move_left" ):
+			# Note there is no max speed while going backwards; this is
+			# intentional.
+			_speed -= _acceleration * delta
+		else:
+			# If the rig is currently moving forward, but there is no user
+			# input, it will slow to a stop.  Note again that this doesn't
+			# happen if the rig is moving backwards; this is also intentional.
+			if _speed > 0.0:
+				_speed -= _acceleration * delta
+			else:
+				_speed = 0.0
+
+	# Move the rig according to its current speed.
+	offset += _speed
