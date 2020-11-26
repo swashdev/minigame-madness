190b099b3ab3b2f5b20be6e44981e2cd988fa1e4
Philip Pavlick <swashdev@pm.me>
2020-11-26 15:37:29 -0600

    [SHOOTER] connect enemy signals to the paths.
    When an `EnemyPath` node spawns a `ShooterBadGuy`, it will connect the
    bad guy's "exploded" signal to an identical signal in the `EnemyPath`,
    which will then be connected to the minigame.

--- mini/shooter/scripts/spawners/enemy_path.gd
+++ mini/shooter/scripts/spawners/enemy_path.gd
@@ -2,6 +2,9 @@ extends Path2D
 # A scene for paths which spawn enemies in the shoot 'em up minigame.
 
 
+# A duplicate of the "exploded" signal in the `ShooterBadGuy` node.
+signal exploded( location )
+
 # Which scene to use for the enemies spawned on the path.
 export (PackedScene) var _enemy
 
@@ -9,4 +12,11 @@ export (PackedScene) var _enemy
 # Spawns an enemy on the path.
 func spawn_enemy():
 	var enemy = _enemy.instance()
+	# Connect the enemy's "exploded" signal to this EnemyPath's signal.
+	enemy.connect( "exploded", self, "_on_enemy_exploded" )
 	add_child( enemy )
+
+
+# Passes up the `ShooterBadGuy` "exploded" signal to the minigame.
+func _on_enemy_exploded( location ):
+	emit_signal( "exploded", location )
