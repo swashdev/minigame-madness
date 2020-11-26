494c9f9eacadcfd71a5833533c885a8cd9c17a53
Philip Pavlick <swashdev@pm.me>
2020-11-26 14:50:34 -0600

    [SHOOTER] add code to spawn bad guys.
    The `ShooterMinigame` now has an `EnemySpawnTimer` which spawns a bad
    guy every half a second.
    
    The `ShooterMinigame` accomplishes this by creating an array of
    `EnemyPath` nodes, which are stored as children of the `Paths` node, and
    randomly selecting one to call `spawn_enemy` on every time
    `EnemySpawnTimer` goes off.

--- mini/shooter/scripts/shooter_minigame.gd
+++ mini/shooter/scripts/shooter_minigame.gd
@@ -5,16 +5,29 @@ extends Minigame
 # The player's projectiles.
 export (PackedScene) var bullet
 
+# An array of paths which can spawn enemies.
+var _paths: Array
+var _path_count: int
 
-# For testing.
+
+# Initialize the minigame by populating `paths` and setting `path_count`
 func _ready():
-	start()
+	_paths = $Paths.get_children()
+	_path_count = _paths.size()
 
 
 # Unlock the player's controls on starting the minigame.
 func start():
 	_unlock_controls = true
 	$Player.allow_movement = true
+	$EnemySpawnTimer.start()
+
+
+# Stop player movement & enemy spawn timers.
+func stop():
+	_unlock_controls = false
+	$Player.allow_movement = false
+	$EnemySpawnTimer.stop()
 
 
 # The player has fired a projectile which must be spawned.
@@ -24,3 +37,9 @@ func _on_Player_shot():
 	shot.position = $Player.position
 	# Offset the shot so it lines up with the player's guns.
 	shot.position.y -= 20
+
+
+# The enemy spawn timer has expired, time to spawn a baddy.
+func _on_EnemySpawnTimer_timeout():
+	var path = _paths[ randi() % _path_count ]
+	path.spawn_enemy()
