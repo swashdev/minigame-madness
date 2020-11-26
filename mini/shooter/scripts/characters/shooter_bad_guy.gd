7194d452265c13f1d4e04e75999be8d6be78174b
Philip Pavlick <swashdev@pm.me>
2020-11-26 14:27:47 -0600

    [SHOOTER] add a functon to blow up bad guys.
    The `explode` function will now cause bad guys to explode.  Also the
    "exploded" signal has been implemented rather than just being implied.
    Nice going, swash.

--- mini/shooter/scripts/characters/shooter_bad_guy.gd
+++ mini/shooter/scripts/characters/shooter_bad_guy.gd
@@ -2,6 +2,9 @@ extends PathFollow2D
 # A script for the bad guys from the shoot 'em up game.
 
 
+# Signals the gameloop that the bad guy has exploded and where.
+signal exploded( location )
+
 # Determines the speed of the enemy ships in units per second.
 const SPEED: float = 40.0
 
@@ -15,13 +18,19 @@ func _process( delta ):
 		queue_free()
 
 
-
-# A projectile has entered the bad guy, causing it to explode.
-func _on_Area2D_body_entered( body ):
-	# Tell the projectile to delete itself.
-	body.queue_free()
+# The bad guy explodes, either as a result of contact with a projectile or the
+# player.
+func explode():
 	# Signal an explosion to the mainloop.  The explosion will be located at
 	# the sprite's global position.
 	emit_signal( "exploded", $AnimatedSprite.global_position )
 	# Delete self, we're done.
 	queue_free()
+
+
+# A projectile has entered the bad guy, causing it to explode.
+func _on_Area2D_body_entered( body ):
+	# Tell the projectile to delete itself.
+	body.queue_free()
+	# Explode.
+	explode()
