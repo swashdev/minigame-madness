361bd294f84ecaa21a72b635018837b3420f885a
Philip Pavlick <swashdev@pm.me>
2020-11-26 17:44:31 -0600

    [SHOOTER] initialize a script for DragonMissile.
    The DragonMissile script declares a signal "big_boom" which tells the
    gameloop when and where to spawn a `BigBoom`, a `blow_up` function which
    emits this signal and then dequeues the missile, and a Y position at
    which the missile will detonate automatically.

---
+++ mini/shooter/scripts/projectiles/dragon_missile.gd
@@ -0,0 +1,23 @@
+extends Projectile2D
+# A script for the DragonMissile from the shoot 'em up game.
+
+
+# This signal instructs the gameloop to spawn a BigBoom at the given y
+# coordinate.
+signal big_boom( y_coordinate )
+
+# At what y coordinate the missile will detonate.
+const BOOM_AT_Y: float = 80.0
+
+
+# `blow_up` when the missile reaches a certain y coordinate.
+func _process( _delta ):
+	if position.y <= BOOM_AT_Y:
+		blow_up()
+
+
+# Signal the mainloop to spawn a BigBoom at the current y coordinates and
+# then delete self.
+func blow_up():
+	emit_signal( "big_boom", position.y )
+	queue_free()
