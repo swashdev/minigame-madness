be17dfaec7a3c52b7588937184f0b830744a18ed
Philip Pavlick <swashdev@pm.me>
2020-11-26 18:49:08 -0600

    [SHOOTER] completely change how the missile works.
    After a hair-pulling amount of testing, I have determined that making
    the missile's explosion a separate entity from the missile itself was
    causing way too many problems and I have decided to change how it works
    by having the missile's collision shape expand in order to destroy the
    enemy ships.  The BigBoom node has been removed entirely.
    
    A lot of work went into this commit and it would be arduous and not
    worth anyone's time to attempt to do atomic changes like you're supposed
    to do with git commits, sorry.

--- mini/shooter/scripts/projectiles/dragon_missile.gd
+++ mini/shooter/scripts/projectiles/dragon_missile.gd
@@ -12,18 +12,26 @@ const BOOM_AT_Y: float = 120.0
 # The speed the missile will go once it's fired.
 const SPEED: float = 200.0
 
+# Whether or not the missile has exploded.
+var _exploded: bool = false
+
 
 # `blow_up` when the missile reaches a certain y coordinate.
 func _process( _delta ):
-	if position.y <= BOOM_AT_Y:
-		blow_up()
+	if not _exploded:
+		if position.y <= BOOM_AT_Y:
+			blow_up()
 
 
 # Signal the mainloop to spawn a BigBoom at the current y coordinates and
 # then delete self.
 func blow_up():
 	emit_signal( "big_boom", position.y )
-	queue_free()
+	_exploded = true
+	$CollisionShape2D.shape.set_radius( 120 )
+	set_speed( 0.0 )
+	hide()
+	$DeathTimer.start()
 
 
 # The missile will be fired.
@@ -31,3 +39,7 @@ func fire():
 	# Enable collision detection.
 	$CollisionShape2D.set_deferred( "disabled", false )
 	set_speed( SPEED )
+
+
+func _on_DeathTimer_timeout():
+	$CollisionShape2D.set_deferred( "disabled", true )
