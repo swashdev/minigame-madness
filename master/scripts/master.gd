fc7df39b74161236114f4758dad6730064e10c22
Philip Pavlick <swashdev@pm.me>
2020-11-27 19:24:11 -0600

    [MASTER] remove version number stuff from Master.
    The `VersionNumberLabel` and all functions and variables related to
    version numbers have been removed awaiting restructuring.

--- master/scripts/master.gd
+++ master/scripts/master.gd
@@ -2,17 +2,6 @@ extends Node
 # The script for the master scene.
 
 
-# The version number.
-enum { YEAR = 20, WEEK = 48, DAY = 4 }
-
-
-# Initialize the master scene.
-func _ready():
-	# Set the version number label.
-	var version = "Version %d.%d.%d" % [YEAR, WEEK, DAY]
-	$VersionNumber.text = version
-
-
 # Main has declared it is ready.
 func _on_Main_ready():
 	$Main.new_game()
