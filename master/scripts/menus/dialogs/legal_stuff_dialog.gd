33e4e71ff9b8f06fab96c573bf9d1c51c7930e80
Philip Pavlick <swashdev@pm.me>
2020-11-27 21:26:32 -0600

    [MASTER] add a script for the LegalStuffDialog.
    The Legal Stuff dialog now has scripting for the buttons that it
    contains.
    
    The "Read the Unlicense" button will toggle the full text of the
    unlicense and a link to the Unlicense website.
    
    The URLs to the Godot Engine license and the Unlicense will emit a
    signal instructing a separate dialog box to warn the user they are
    attempting to open an external URL.

---
+++ master/scripts/menus/dialogs/legal_stuff_dialog.gd
@@ -0,0 +1,29 @@
+extends WindowDialog
+# A script for the "Legal Stuff" dialog box.
+
+
+# This signal will send an instruction to a different dialog box to warn the
+# user they are attempting to open a URI.
+signal open_external( uri )
+
+
+# For testing:
+func _ready():
+	popup_centered()
+# The "Read the Unlicense" button has been toggled, which will show or hide
+# the Unlicense and a button to the Unlicense website.
+func _on_MMLicenseButton_toggled( button_pressed ):
+	$ScrollContainer/VBoxContainer/MMLicenseText.visible = button_pressed
+	$ScrollContainer/VBoxContainer/UnlicenseLink.visible = button_pressed
+
+
+# The link to the Unlicense website has been pressed, so we're going to send
+# an instruction to the operating system to visit that website.
+func _on_UnlicenseLink_pressed():
+	emit_signal( "open_external", "https://unlicense.org/" )
+
+
+# The link to the Godot license page has been pressed, so we're going to send
+# an instruction to the operating system to visit that website.
+func _on_GodotLicenseLink_pressed():
+	emit_signal( "open_external", "https://godotengine.org/license" )
