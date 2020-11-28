c8551e1d556d5c45be1bdb5bef1bc1d71821934b
Philip Pavlick <swashdev@pm.me>
2020-11-27 19:22:34 -0600

    [MASTER] add a signal for main menu buttons.
    `Master` will now respond to the "button_pressed" signal from
    `MainMenu`.  Currently the function which does the response is empty.

--- master/scripts/master.gd
+++ master/scripts/master.gd
@@ -16,3 +16,9 @@ func _ready():
 # Main has declared it is ready.
 func _on_Main_ready():
 	$Main.new_game()
+
+
+# A button has been pressed on the main menu which requires Master's
+# attention.
+func _on_MainMenu_button_pressed( button ):
+	pass # Replace with function body.
