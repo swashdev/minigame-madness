6aa4e563bf7d60468a4edef2ca81d4ee907156e2
Philip Pavlick <swashdev@pm.me>
2020-12-01 15:56:59 -0600

    [BALL] respawn the ball when it signals collision.

--- mini/ball_tribute/scripts/ball_tribute.gd
+++ mini/ball_tribute/scripts/ball_tribute.gd
@@ -5,3 +5,8 @@ extends Minigame
 # Move the player back to the spawn point.
 func respawn():
 	$Ball.respawn( $PlayerStart.position )
+
+
+# The ball has collided with a wall.
+func _on_Ball_collided( location ):
+	respawn()
