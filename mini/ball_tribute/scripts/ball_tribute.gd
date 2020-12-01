416fdd05e31fa1eea2a0210321478798ca90b83d
Philip Pavlick <swashdev@pm.me>
2020-12-01 16:24:10 -0600

    [BALL] spawn a ghost on collision.
    The ball tribute minigame will now spawn a ghost after respawning the
    ball when the ball reports a collision.

--- mini/ball_tribute/scripts/ball_tribute.gd
+++ mini/ball_tribute/scripts/ball_tribute.gd
@@ -2,11 +2,21 @@ extends Minigame
 # A script for the Ball tribute.
 
 
+export (PackedScene) var _ghost
+
+
 # Move the player back to the spawn point.
 func respawn():
 	$Ball.respawn( $PlayerStart.position )
 
 
 # The ball has collided with a wall.
-func _on_Ball_collided( location ):
+func _on_Ball_collided( location, degrees ):
+	# Respawn the ball.
 	respawn()
+
+	# Create a ghost ball at the player's previous location.
+	var ghost = _ghost.instance()
+	ghost.position = location
+	ghost.rotation_degrees = degrees
+	add_child( ghost )
