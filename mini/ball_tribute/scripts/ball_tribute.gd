c61da5209d112d4fc42cd46efc91d53f15618b2d
Philip Pavlick <swashdev@pm.me>
2020-12-01 16:43:50 -0600

    [BALL] move ghost spawn code to respawn function.

--- mini/ball_tribute/scripts/ball_tribute.gd
+++ mini/ball_tribute/scripts/ball_tribute.gd
@@ -16,17 +16,17 @@ func stop():
 
 
 # Move the player back to the spawn point.
-func respawn():
+func respawn( location, degrees ):
 	$Ball.respawn( $PlayerStart.position )
-
-
-# The ball has collided with a wall.
-func _on_Ball_collided( location, degrees ):
-	# Respawn the ball.
-	respawn()
 
 	# Create a ghost ball at the player's previous location.
 	var ghost = _ghost.instance()
 	ghost.position = location
 	ghost.rotation_degrees = degrees
 	add_child( ghost )
+
+
+# The ball has collided with a wall.
+func _on_Ball_collided( location, degrees ):
+	# Respawn the ball.
+	respawn( location, degrees )
