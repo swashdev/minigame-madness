d5c280da9ff15879df740a9821ef912c46323ae2
Philip Pavlick <swashdev@pm.me>
2020-12-17 14:56:53 -0600

    [SHARED] re-implement projectiles as kinematic.
    As a reflection of previous changes to the game to improve collision
    detection, projectiles will now be `KinematicBody2D` objects.
    
    Code has been added to the projectile script to handle movement and
    signal collisions, and an exported variable `_disappear_on_collision`
    has been added which defaults to true which will determine if the
    projectile disappears on detecting a collision.

--- shared/scripts/projectiles/projectile.gd
+++ shared/scripts/projectiles/projectile.gd
@@ -1,8 +1,13 @@
 class_name Projectile2D
-extends RigidBody2D
+extends KinematicBody2D
 # A generic class for in-game projectiles of arbitrary speed, direction,
 # shape, and size.
 
+
+# A signal to indicate that a collision has occurred.  `data` is a
+# `KinematicCollision2D` used to provide the minigame with information about
+# the collision.
+signal collided( data )
 
 # The rate of speed of the projectile, in pixels.
 export (float) var _speed = 0.0 setget set_speed, get_speed
@@ -13,6 +18,12 @@ export (float) var _direction = 0.0 setget set_direction, get_direction
 # The rate of spin for the projectile in degrees.
 export (float) var spin_degrees = 0.0 setget set_spin, get_spin
 
+# If true, the projectile will remove itself upon detecting a collision.
+export (bool) var _disappear_on_collision = true
+
+# The current velocity of the projectile.
+var _velocity: Vector2 setget set_velocity, get_velocity
+
 
 # Initializes the projectile by setting its linear velocity based on the
 # speed & direction parameters.
@@ -22,7 +33,16 @@ func _init():
 
 # The default process will move the projectile in space and, if applicable,
 # spin it.
-func _process( delta ):
+func _physics_process( delta ):
+	var collision = move_and_collide( _velocity * delta )
+
+	# If a collision is detected, signal this.  If applicable, the projectile
+	# will also disappear.
+	if collision:
+		emit_signal( "collided", collision )
+		if _disappear_on_collision:
+			queue_free()
+
 	if spin_degrees != 0.0:
 		$Sprite.rotation_degrees += spin_degrees * delta
 
@@ -58,7 +78,11 @@ func get_spin() -> float:
 func set_velocity( new_speed = _speed, new_direction = _direction ):
 	_speed = new_speed
 	_direction = new_direction
-	linear_velocity = Vector2( _speed, 0 ).rotated( deg2rad( _direction ) )
+	_velocity = Vector2( _speed, 0 ).rotated( deg2rad( _direction ) )
+
+
+func get_velocity() -> Vector2:
+	return _velocity
 
 
 # Disappear when leaving the screen.
