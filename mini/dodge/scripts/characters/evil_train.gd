f3d98dc3c232b2c855d2598301f7b0c386b3cace
swashberry <swashdev@pm.me>
2021-05-05 21:22:47 -0500

    [DODGE] create a scene & script for evil train.
    The evil train is a blatant copy-paste of the normal train rotated and
    with a different script.

---
+++ mini/dodge/scripts/characters/evil_train.gd
@@ -0,0 +1,17 @@
+extends Area2D
+# A script for the evil train from the "Dodge!" minigame.
+
+
+# Stores whether or not the train has been released.
+var _unlock_controls: bool = false setget unlock
+
+
+# The mainloop: Moves the train along the track when it's released.
+func _process( delta ):
+	if _unlock_controls:
+		position.x -= 200 * delta
+
+
+# Unlocks the controls for the train.
+func unlock( switch: bool = true ):
+	_unlock_controls = switch
