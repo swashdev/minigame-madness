6535f8624324aabc1ea5c379f41f3359093e5f89
swashberry <swashdev@pm.me>
2021-06-07 16:55:57 -0500

    [PITFALL] add Indiana Jumpman to the game.
    The IndianaJumpman scene represents the player character for this
    minigame.  He's a basic platforming character.
    
    This commit also changes all of the Area2D nodes in the game into
    StaticBody2D nodes so that platforming will work.

---
+++ mini/pitfall/scripts/characters/indiana_jumpman.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+# A script for the jumpman character from "Get Across!"
+
+
+# Determines the player's jump, gravity, and movement speed.
+const JUMP_SPEED: float = -20.0
+const GRAVITY: float = 40.0
+const MOVE_SPEED: float = 20.0
+
+
+# The player's current momentum.
+var momentum: Vector2
+
+
+# The player's mainloop.  Handles basic movement.
+func _physics_process( delta ):
+	var grounded = is_on_floor()
+	
+	# Zero out the player's horizontal momentum.
+	momentum.x = 0.0
+
+	# Handle left and right movement.
+	if Input.is_action_pressed( "move_left" ):
+		$Sprite.flip_h = true
+		momentum.x -= MOVE_SPEED
+		if grounded:
+			$Sprite.animation = "walking"
+	if Input.is_action_pressed( "move_right" ):
+		$Sprite.flip_h = false
+		momentum.x += MOVE_SPEED
+		if grounded:
+			$Sprite.animation = "walking"
+
+	if not grounded:
+		momentum.y += GRAVITY * delta
+	else:
+		momentum.y = 0.0
+		if Input.is_action_just_pressed( "action" ):
+			momentum.y = JUMP_SPEED
+			$Sprite.animation = "jumping"
+		# If the player is grounded and is not jumping or moving, reset his
+		# animation.
+		elif momentum.x == 0.0:
+			$Sprite.animation = "default"
+
+	# Move the player according to what we've determined above.
+# warning-ignore:return_value_discarded
+	move_and_slide( momentum, Vector2.UP )
+
+	# Clamp the player's position to within the game window.
+	position.x = clamp( position.x, 4.0, 76.0 )
