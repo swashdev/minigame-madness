a52303950ce84aef9cb04db804c4e5dda0a6976c
swashberry <swashdev@pm.me>
2021-07-01 12:11:40 -0500

    [BIGRIGS] add movement for the "2nd player" rig.
    The second truck in the "Race that Rig!" minigame will now move when the
    game starts and continue moving until the end of the minigame.  Although
    the rig not moving was originally part of the joke, people seemed to
    find this confusing.
    
    The rig will move at a constant speed until the end of the minigame
    (which should coincide with the rig reaching the end of the track),
    mimicking the behavior of the AI truck from Big Rigs: Over the Road
    Racing.

--- mini/big_rigs/scripts/big_rig_minigame.gd
+++ mini/big_rigs/scripts/big_rig_minigame.gd
@@ -2,6 +2,13 @@ extends Minigame
 # A script for the "Race that Rig!" minigame.
 
 
+# Mainloop for the minigame.  Moves the second player rig.
+func _process( delta ):
+	if _unlock_controls:
+		if $RigPath/Rig2.unit_offset < 1.0:
+			$RigPath/Rig2.unit_offset += 0.25 * delta
+
+
 # Upon starting, enable movement in the rigs.
 func start():
 	_unlock_controls = true
@@ -11,4 +18,5 @@ func start():
 # Rig1 has reached the end of the course and the player therefore wins.
 func _on_Rig1_finished():
 	emit_signal( "won" )
+	_unlock_controls = false
 	$TextureRect.show()
