98f6c9bcc7c83ded5979c1c55fc9b83e9e821793
swashberry <swashdev@pm.me>
2022-04-06 16:57:57 -0500

    [DANCE] add a trigger for removing the arrow.
    Arrows will now be despawned by pressing the arrow key which corresponds
    with the arrow's direction.
    
    As a side-effect of this commit, scenes which use the script in
    dance_arrow.gd will now be able to select a direction from the editor,
    if so desired.

--- mini/dance/scripts/ui/dance_arrow.gd
+++ mini/dance/scripts/ui/dance_arrow.gd
@@ -1,3 +1,4 @@
+tool
 extends Sprite
 # an arrow which can point in one of four directions and which changes color
 # based on its direction.
@@ -27,7 +28,22 @@ const DEGREES: Dictionary = \
 }
 
 
+# Actions which might cause the arrow to trigger, based on its direction.
+const TRIGGERS: Dictionary = \
+{
+	UP: "move_up",
+	RIGHT: "move_right",
+	DOWN: "move_down",
+	LEFT: "move_left",
+}
+
+
 const FALL_SPEED: float = 600.0
+
+
+# Determines what direction the arrow is pointing.
+export(int, "Up", "Down", "Left", "Right") onready var _direction: int = 0 \
+		setget set_direction, get_direction
 
 
 # Mainloop for the dance arrows.
@@ -39,9 +55,14 @@ func _process( delta ):
 	else:
 		position.y += FALL_SPEED * delta
 
+	# if the arrow's trigger has been pressed, delet.
+	if Input.is_action_just_pressed( TRIGGERS[_direction] ):
+		queue_free()
+
 
 # sets the arrow's direction.
 func set_direction( direction: int ):
+	_direction = direction
 	if DEGREES.has( direction ):
 		set_rotation_degrees( DEGREES[direction] )
 	if PALETTE.has( direction ):
@@ -51,3 +72,6 @@ func set_direction( direction: int ):
 	if Engine.editor_hint:
 		property_list_changed_notify()
 
+
+func get_direction() -> int:
+	return _direction
