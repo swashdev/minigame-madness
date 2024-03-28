5dec4c7adc2bce3a520540d710d9c8da07b44cec
swashberry <swashdev@pm.me>
2024-03-28 18:09:52 -0500

    [MASTER] bump version number to 0.14-dev.1

--- master/singletons/version.gd
+++ master/singletons/version.gd
@@ -6,9 +6,9 @@ extends Node
 # standard 2.0.0.  Note, however, that Minigame Madness's version numbering
 # system is not compliant with the Semantic Versioning standard.
 const MAJOR: int = 0
-const MINOR: int = 13
-const PATCH: int = 5
-const BUILD: String = "stable"
+const MINOR: int = 14
+const PATCH: int = 0
+const BUILD: String = "dev.1"
 
 
 func _to_string() -> String:
