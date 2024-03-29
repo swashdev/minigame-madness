b47b835a4dfa7c626b06edbb6c589ceb7c82267d
swashberry <swashdev@pm.me>
2024-03-28 20:23:01 -0500

    [GOALIE] add a script for the goalie.
    A script allowing the goalie to move up and down like a Pong paddle has
    been added.

---
+++ mini/goalie/scripts/characters/goalie.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+# A goalie for the soccer game.
+
+
+const BASE_SPEED: int = 700
+
+
+var unlock_movement: bool = false
+
+
+# The goalie's main process.
+func _physics_process(delta):
+	if unlock_movement:
+		# Figure out if we're moving in this frame.
+		var up_strength: float = Input.get_action_strength("move_up")
+		var down_strength: float = Input.get_action_strength("move_down")
+		var speed = down_strength - up_strength
+
+		# Move the goalie.
+		position.y += speed * BASE_SPEED * delta
+		# Constrain the goalie's movement.
+		if position.y < 50.0:
+			position.y = 50.0
+		elif position.y > 430.0:
+			position.y = 430.0
