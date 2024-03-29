a33ce26f36f7051a559a80d91d63bc54bd920a48
swashberry <swashdev@pm.me>
2024-03-28 21:11:51 -0500

    [GOALIE] connect ball signals.
    The "Defend!" minigame now has a script which starts and stops the game
    and reacts to the ball's signals.

---
+++ mini/goalie/goalie_minigame.gd
@@ -0,0 +1,26 @@
+extends Minigame
+# A minigame that doesn't in any way resemble Pong.
+
+
+# Starts the minigame.
+func start():
+	$Goalie.unlock_movement = true
+	$Ball.start()
+
+
+# Stops the minigame.
+func stop():
+	$Goalie.unlock_movement = false
+	$Ball.out_of_play = true
+
+
+# The ball has passed the goal, and the player has lost.
+func _on_Ball_passed_goal():
+	stop()
+	emit_signal("lost")
+
+
+# The ball has moved out-of-bounds, triggering an early win.
+func _on_Ball_out_of_bounds():
+	stop()
+	emit_signal("won")
