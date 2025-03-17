ca93dafd230be2b513fa74a56e7c29657c58d6fa
swashberry <swashdev@pm.me>
2025-03-17 17:36:34 -0500

    Update web build for 0.14_1-beta.1

--- index.audio.worklet.js
+++ index.audio.worklet.js
@@ -1,44 +1,43 @@
-/**************************************************************************/
-/*  audio.worklet.js                                                      */
-/**************************************************************************/
-/*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
-/**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
-/*                                                                        */
-/* Permission is hereby granted, free of charge, to any person obtaining  */
-/* a copy of this software and associated documentation files (the        */
-/* "Software"), to deal in the Software without restriction, including    */
-/* without limitation the rights to use, copy, modify, merge, publish,    */
-/* distribute, sublicense, and/or sell copies of the Software, and to     */
-/* permit persons to whom the Software is furnished to do so, subject to  */
-/* the following conditions:                                              */
-/*                                                                        */
-/* The above copyright notice and this permission notice shall be         */
-/* included in all copies or substantial portions of the Software.        */
-/*                                                                        */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
-/**************************************************************************/
+/*************************************************************************/
+/*  audio.worklet.js                                                     */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
 
 class RingBuffer {
-	constructor(p_buffer, p_state, p_threads) {
+	constructor(p_buffer, p_state) {
 		this.buffer = p_buffer;
 		this.avail = p_state;
-		this.threads = p_threads;
 		this.rpos = 0;
 		this.wpos = 0;
 	}
 
 	data_left() {
-		return this.threads ? Atomics.load(this.avail, 0) : this.avail;
+		return Atomics.load(this.avail, 0);
 	}
 
 	space_left() {
@@ -56,16 +55,10 @@ class RingBuffer {
 			to_write -= high;
 			this.rpos = 0;
 		}
-		if (to_write) {
-			output.set(this.buffer.subarray(this.rpos, this.rpos + to_write), from);
-		}
+		output.set(this.buffer.subarray(this.rpos, this.rpos + to_write), from);
 		this.rpos += to_write;
-		if (this.threads) {
-			Atomics.add(this.avail, 0, -output.length);
-			Atomics.notify(this.avail, 0);
-		} else {
-			this.avail -= output.length;
-		}
+		Atomics.add(this.avail, 0, -output.length);
+		Atomics.notify(this.avail, 0);
 	}
 
 	write(p_buffer) {
@@ -84,19 +77,14 @@ class RingBuffer {
 			this.buffer.set(low);
 			this.wpos = low.length;
 		}
-		if (this.threads) {
-			Atomics.add(this.avail, 0, to_write);
-			Atomics.notify(this.avail, 0);
-		} else {
-			this.avail += to_write;
-		}
+		Atomics.add(this.avail, 0, to_write);
+		Atomics.notify(this.avail, 0);
 	}
 }
 
 class GodotProcessor extends AudioWorkletProcessor {
 	constructor() {
 		super();
-		this.threads = false;
 		this.running = true;
 		this.lock = null;
 		this.notifier = null;
@@ -112,31 +100,24 @@ class GodotProcessor extends AudioWorkletProcessor {
 	}
 
 	process_notify() {
-		if (this.notifier) {
-			Atomics.add(this.notifier, 0, 1);
-			Atomics.notify(this.notifier, 0);
-		}
+		Atomics.add(this.notifier, 0, 1);
+		Atomics.notify(this.notifier, 0);
 	}
 
 	parse_message(p_cmd, p_data) {
 		if (p_cmd === 'start' && p_data) {
 			const state = p_data[0];
 			let idx = 0;
-			this.threads = true;
 			this.lock = state.subarray(idx, ++idx);
 			this.notifier = state.subarray(idx, ++idx);
 			const avail_in = state.subarray(idx, ++idx);
 			const avail_out = state.subarray(idx, ++idx);
-			this.input = new RingBuffer(p_data[1], avail_in, true);
-			this.output = new RingBuffer(p_data[2], avail_out, true);
+			this.input = new RingBuffer(p_data[1], avail_in);
+			this.output = new RingBuffer(p_data[2], avail_out);
 		} else if (p_cmd === 'stop') {
-			this.running = false;
+			this.runing = false;
 			this.output = null;
 			this.input = null;
-		} else if (p_cmd === 'start_nothreads') {
-			this.output = new RingBuffer(p_data[0], p_data[0].length, false);
-		} else if (p_cmd === 'chunk') {
-			this.output.write(p_data);
 		}
 	}
 
@@ -158,10 +139,7 @@ class GodotProcessor extends AudioWorkletProcessor {
 			if (this.input_buffer.length !== chunk) {
 				this.input_buffer = new Float32Array(chunk);
 			}
-			if (!this.threads) {
-				GodotProcessor.write_input(this.input_buffer, input);
-				this.port.postMessage({ 'cmd': 'input', 'data': this.input_buffer });
-			} else if (this.input.space_left() >= chunk) {
+			if (this.input.space_left() >= chunk) {
 				GodotProcessor.write_input(this.input_buffer, input);
 				this.input.write(this.input_buffer);
 			} else {
@@ -178,9 +156,6 @@ class GodotProcessor extends AudioWorkletProcessor {
 			if (this.output.data_left() >= chunk) {
 				this.output.read(this.output_buffer);
 				GodotProcessor.write_output(output, this.output_buffer);
-				if (!this.threads) {
-					this.port.postMessage({ 'cmd': 'read', 'data': chunk });
-				}
 			} else {
 				this.port.postMessage('Output buffer has not enough frames! Skipping output frame.');
 			}
