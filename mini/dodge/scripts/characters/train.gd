eddf94d13e1da87fecb9526f4b1d9993ae4c0a30
swashberry <swashdev@pm.me>
2025-07-06 00:10:36 -0500

    [DODGE] Allow smooth player movement.
    The player will now move up and down mostly organically, rather than
    snapping between lanes.

--- mini/dodge/scripts/characters/train.gd
+++ mini/dodge/scripts/characters/train.gd
@@ -7,27 +7,21 @@ enum Track { TOP = 0, MIDDLE = 1, BOTTOM = 2 }
 
 
 # The space between each track.
-const TRACK_SPACE: float = 112.0
+const MOVEMENT_SPEED: float = 300.0
 
 
 # Stores whether or not controls for the train have been unlocked.
 var _unlock_controls: bool = false setget unlock
 
-# Stores which track the train is currently on.
-var current_track: int = Track.MIDDLE
-
-
 # The mainloop: Moves the train up and down the tracks on player input.
 func _process( _delta ):
 	if _unlock_controls:
-		if Input.is_action_just_pressed( "move_up" ):
-			if current_track > Track.TOP:
-				current_track -= 1
-				position.y -= TRACK_SPACE
-		elif Input.is_action_just_pressed( "move_down" ):
-			if current_track < Track.BOTTOM:
-				current_track += 1
-				position.y += TRACK_SPACE
+		var velocity = 0.0
+		if Input.is_action_pressed( "move_up" ):
+			velocity -= MOVEMENT_SPEED
+		if Input.is_action_pressed( "move_down" ):
+			velocity += MOVEMENT_SPEED
+		position.y += velocity * _delta
 
 
 # Unlocks the controls for the train.
