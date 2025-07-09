bf6e889e3ddfbfb99d807a5bed8ca9a4330367d0
swashberry <swashdev@pm.me>
2025-07-09 16:57:38 -0500

    [JUMP] Add code to move the pushers.
    The pushers will now move to the left for as long as the minigame is
    running, and will attempt to push the player off the left side of the
    screen.

--- mini/jump/jump_minigame.gd
+++ mini/jump/jump_minigame.gd
@@ -4,6 +4,7 @@ extends Minigame
 
 const GRAVITY: float = 1024.0
 const JUMP_VELOCITY: float = 512.0
+const PUSHER_VELOCITY: Vector2 = Vector2(-320.0, 0.0)
 
 
 # The player's current velocity.
@@ -16,21 +17,38 @@ var grounded: bool = false
 # The player character.
 onready var player: KinematicBody2D = $Player
 
+# The pushers (obstacles)
+onready var pushers: Array = $Pushers.get_children()
+
 
 # The mainloop.
 func _physics_process(delta):
+	var collision: KinematicCollision2D
+	var remainder: Vector2
+
 	# The player is always pulled down due to gravity, even if the game isn't
 	# running.
 	velocity.y += GRAVITY * delta
+	velocity.x = 0.0
 
-	# If the controls have been unlocked, check to see if the player is
-	# grounded and allow them to jump.
 	if _unlock_controls:
+		# If the  player is grounded, allow them to jump.
 		if grounded:
 			if Input.is_action_just_pressed("action") \
 			or Input.is_action_just_pressed("move_up"):
 				velocity.y -= JUMP_VELOCITY
 				print_debug("Jump button pressed!  Y velocity = " + str(velocity.y))
+
+		# Advance the pushers.
+		for pusher in pushers:
+			collision = pusher.move_and_collide(PUSHER_VELOCITY * delta)
+			if collision:
+				# If the pusher collides with the player, push the player
+				# along.  Also, finish the pusher's own movement to keep the
+				# velocity consistent.
+				remainder = collision.get_remainder()
+				player.position += remainder
+				pusher.position += remainder
 
 	# Move the player.
 	velocity = player.move_and_slide(velocity, Vector2.UP)
