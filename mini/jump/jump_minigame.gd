635648d5a75d60fb791043710d51a7f79b95930e
swashberry <swashdev@pm.me>
2025-07-13 01:58:00 -0500

    [JUMP] Add a nifty jumping animation.

--- mini/jump/jump_minigame.gd
+++ mini/jump/jump_minigame.gd
@@ -6,22 +6,42 @@ const GRAVITY: float = 2048.0
 const JUMP_VELOCITY: float = 768.0
 const PUSHER_VELOCITY: Vector2 = Vector2(-660.0, 0.0)
 
+# A little animation when the player jumps.
+const ROTATION_SPEED: float = 360.0
+const ROTATION_DEGREES: float = 90.0
+
 
 # The player's current velocity.
 var velocity: Vector2 = Vector2.ZERO
 
 # Whether or not the player is currently on a floor.
 var grounded: bool = false
+# Keeps track of the player's rotation animation.
+var jumped: bool = false
+var rotation_animation: float = 0.0
 
 
 # The player character.
 onready var player: KinematicBody2D = $Player
+onready var player_sprite: ColorRect = $Player/ColorRect
 
 # The pushers (obstacles)
 onready var pushers: Array = $Pushers.get_children()
 
 
 # The mainloop.
+func _process(delta):
+	var rotation_frame: float
+	if _unlock_controls and jumped:
+		rotation_frame = ROTATION_SPEED * delta
+		rotation_animation += rotation_frame
+		player_sprite.rect_rotation += rotation_frame
+		if rotation_animation > ROTATION_DEGREES:
+			player_sprite.rect_rotation = 0.0
+			rotation_animation = 0.0
+			jumped = false
+
+
 func _physics_process(delta):
 	var collision: KinematicCollision2D
 	var remainder: Vector2
@@ -34,9 +54,11 @@ func _physics_process(delta):
 	if _unlock_controls:
 		# If the  player is grounded, allow them to jump.
 		if grounded:
+			jumped = false
 			if Input.is_action_just_pressed("action") \
 			or Input.is_action_just_pressed("move_up"):
 				velocity.y -= JUMP_VELOCITY
+				jumped = true
 
 		# Advance the pushers.
 		for pusher in pushers:
@@ -58,3 +80,8 @@ func _physics_process(delta):
 	velocity = player.move_and_slide(velocity, Vector2.UP)
 	# Detect if the player is grounded.
 	grounded = player.is_on_floor()
+
+
+func stop():
+	.stop()
+	player_sprite.rect_rotation = 0.0
