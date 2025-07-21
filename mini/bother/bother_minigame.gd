e8da45cf31f3c7c55cdc4d8c194b19f42ee239f5
swashberry <swashdev@pm.me>
2025-07-21 15:12:55 -0500

    [BOTHER] Initialize the patting animation.

--- mini/bother/bother_minigame.gd
+++ mini/bother/bother_minigame.gd
@@ -9,7 +9,15 @@ const POKES_TO_WIN: int = 16
 var pokes: int = 0
 
 
+# A progress bar showing how many pokes have happened
 onready var progress_bar = $ProgressBar
+
+# The perpetrator, I mean player
+onready var hand: Sprite = $Hand
+# The victim, I mean critter
+onready var critter: Sprite = $Cat
+# Where to put the hand during pats
+onready var pat_point: Vector2 = $Cat/PatPoint.global_position
 
 
 func _ready():
@@ -21,5 +29,9 @@ func _input(event):
 		if event.is_action_pressed("action", false):
 			pokes += 1
 			progress_bar.set_value(pokes)
+			hand.position = pat_point
 			if pokes >= POKES_TO_WIN:
 				emit_signal("won")
+		elif event.is_action_released("action"):
+			hand.position.y -= 100
+			hand.position.x += 25
