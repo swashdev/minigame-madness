808a259b0131d04ab13a812a78ef9c55013194a5
swashberry <swashdev@pm.me>
2025-07-21 15:18:40 -0500

    [BOTHER] Animate the critter's eyes.

--- mini/bother/bother_minigame.gd
+++ mini/bother/bother_minigame.gd
@@ -3,12 +3,19 @@ extends Minigame
 
 
 const POKES_TO_WIN: int = 16
+const POKES_TO_ANGER: int = 12
 const SQUISH_SCALE: Vector2 = Vector2(1.1, 0.9)
 const NORMAL_SCALE: Vector2 = Vector2(1, 1)
 
 
 # The number of times the cat has been poked.
 var pokes: int = 0
+
+# Textures to be used for the critter's face.
+var face_awake: Texture = \
+		preload("res://mini/bother/images/faces/awake.png")
+var face_angry: Texture = \
+		preload("res://mini/bother/images/faces/angry.png")
 
 
 # A progress bar showing how many pokes have happened
@@ -18,6 +25,8 @@ onready var progress_bar = $ProgressBar
 onready var hand: Sprite = $Hand
 # The victim, I mean critter
 onready var critter: Sprite = $Cat
+# The critter's face
+onready var face: Sprite = $Cat/Face
 # Where to put the hand during pats
 onready var pat_point: Vector2 = $Cat/PatPoint.global_position
 
@@ -33,7 +42,11 @@ func _input(event):
 			progress_bar.set_value(pokes)
 			critter.scale = SQUISH_SCALE
 			hand.position = pat_point
-			if pokes >= POKES_TO_WIN:
+			if pokes == 1:
+				face.set_texture(face_awake)
+			elif pokes == POKES_TO_ANGER:
+				face.set_texture(face_angry)
+			elif pokes >= POKES_TO_WIN:
 				emit_signal("won")
 		elif event.is_action_released("action"):
 			hand.position.y -= 100
